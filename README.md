4c5bf17409d814b22f1808bfefb20d34e6807081
Bernardo López <bernardolopezf.dev@gmail.com>
2026-01-31 23:18:19 -0500

    Add new files

---
+++ README.md
@@ -0,0 +1,4 @@
+Sentencia SELECT: 
+    Sentencia de seleccion. Me permite consultar los datos de una tabla. 
+
+    SELECT * FROM (nombre de la tabla) => nos devuelve todos los datos de la tabla.
---
+++ Statements/select.sql
@@ -0,0 +1,25 @@
+SELECT * FROM users;
+SELECT name FROM users;
+SELECT name, age FROM users;
+
+/*
+
+1️. Obtener el id, título y año de estreno de todas las películas.
+
+2. Obtener todo el contenido de la tabla "countries".
+
+3. Obtener el nombre y fecha de nacimiento todas las personas almacenada en la tabla "people".
+
+4. Obtener todos los géneros de películas disponibles en la tabla "genres".
+
+5. ️Obtener el nombre, cantidad de espectadores y recaudación de todas las películas.
+
+6. Obtener el nombre de los estudios cinematográficos almacenados en la tabla "studios".
+
+*/ 
+
+SELECT movie_id, title, release_year FROM movies;
+SELECT * FROM countries;
+SELECT name, birth_date FROM people;
+SELECT genre_name FROM genres;
+SELECT title, total_viewers, total_revenue FROM movies;
